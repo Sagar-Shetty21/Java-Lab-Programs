2c9c8b7e451d6e6c246968a8cc93e8c81c497950
sagar <5agarm5hetty05@gmail.com>
2022-11-22 16:36:59 +0530

    added 3 programs

---
+++ Program4/src/Program4/PayoutOfBoundsException.java
@@ -0,0 +1,9 @@
+package Program4;
+import java.util.*;
+
+class PayoutOfBoundsException extends Exception {
+	PayoutOfBoundsException(String msg){
+		System.out.println("Pay Out of Bounds Exception : " + msg);
+	}
+}
+
